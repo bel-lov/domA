a8e0c29347a924379b494032e008045570618e0a
bel-lov <belou.lyuba@yandex.ru>
2022-12-18 20:03:26 +0300

    modified all

--- src/js/app.js
+++ src/js/app.js
@@ -1,34 +1,34 @@
-export default class Board {
-    constructor(size) {
-        this.size = size;
-    }
+export default class NewField {
+  constructor(board) {
+    this.board = board;
+  }
 
-    start() {
-        const boardBox = document.getElementById('board_box');
-        for (let i = 0; i < this.size ** 2; i += 1) {
-            const cell = document.createElement('div');
-            cell.classList = 'cell';
-            boardBox.appendChild(cell);
-        }
-        const cellList = document.getElementsByClassName('cell');
-        let index;
-        let lastIndex = 0;
-        const min = 0;
-        const max = this.size ** 2 - 1;
-        setInterval(() => {
-            index = Math.floor(min + Math.random() * (max + 1 - min));
-            if (index === lastIndex) {
-                index += 1;
-                if (index >= max) {
-                    index = 0;
-                }
-            }
-            cellList[index].innerHTML = '<img src="goblin.png">';
-            cellList[lastIndex].innerHTML = '';
-            lastIndex = index;
-        }, 800);
+  start() {
+    const plaingField = document.querySelector('.board');
+    for (let i = 0; i < this.board ** 2; i += 1) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell')
+      plaingField.prepend(cell);
     }
+    const cellList = document.getElementsByClassName('cell');
+    let index;
+    let lastIndex = 0;
+    const min = 0;
+    const max = this.board ** 2 - 1;
+    setInterval(() => {
+      index = Math.floor(min + Math.random() * (max + 1 - min));
+      if (index === lastIndex) {
+        index += 1;
+        if (index >= max) {
+          index = 0;
+        }
+      }
+      cellList[index].innerHTML = '<img src="goblin.png">';
+      cellList[lastIndex].innerHTML = '';
+      lastIndex = index;
+    }, 1000);
+  }
 }
 
-const board = new Board(4);
-board.start();
+const play = new NewField(4);
+play.start();
